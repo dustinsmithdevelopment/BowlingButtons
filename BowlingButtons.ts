913cbba8014a086d79f7c9e786bf5488645fbf8b
TechGryphon <TechGryphon@gmail.com>
2025-03-14 11:41:49 -0500

    Margin

--- BowlingButtons.ts
+++ BowlingButtons.ts
@@ -7,22 +7,38 @@ const BACKGROUND_COLOR = "#ffffff";
 class BowlingButtons extends UIComponent<typeof BowlingButtons> {
   panelHeight = 200;
   panelWidth = 500;
+    private buttonStyling = {width: 100, height: 100, borderRadius: 20, margin: 4};
   static propsDefinition = {};
   initializeUI(): UINode {
     return View({
       children: [
           View({children: [
-              Pressable({children: [Text({text: "Start"})]}),
+              Pressable({children: [
+                  Text({text: "Start",
+                      style: {color: "white", height: '100%', textAlignVertical: 'center', textAlign: 'center'}})]
+                  , style: {backgroundColor: "green", ...this.buttonStyling}}),
             ]}),
           View({children: [
-              Pressable({children: [Text({text: "Reset"})]}),
-              Pressable({children: [Text({text: "Ball Glitched"})]})
-            ],style:{display:"flex",flexDirection:"column",justifyContent:"center"}}),
+              Pressable({children: [
+                  Text({text: "Reset",
+                      style: {color: "white", height: '100%', textAlignVertical: 'center', textAlign: 'center'}})],
+                  style: {backgroundColor: "red", ...this.buttonStyling}}),
+              Pressable({children: [
+                  Text({text: "Ball Glitched",
+                      style: {color: "black", height: '100%', textAlignVertical: 'center', textAlign: 'center'}})],
+                  style: {backgroundColor: "yellow", ...this.buttonStyling}})],
+              style:{display:"flex",flexDirection:"column",justifyContent:"center"}}),
           View({children: [
-              Pressable({children: [Text({text: "Join"})]}),
-              Pressable({children: [Text({text: "Skip Turn"})]})
-            ],style:{display:"flex",flexDirection:"column",justifyContent:"center"}})
-      ], style: {backgroundColor: BACKGROUND_COLOR, display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', height: '100%'}
+              Pressable({children: [
+                  Text({text: "Join",
+                      style: {color: "white", width: 100, height: 100, borderRadius: 20}})],
+                  style: {backgroundColor: "blue", ...this.buttonStyling}}),
+              Pressable({children: [
+                  Text({text: "Skip Turn",
+                      style: {height: '100%', textAlignVertical: 'center', textAlign: 'center'}})],
+                  style: {backgroundColor: "purple", ...this.buttonStyling}})],
+              style:{display:"flex",flexDirection:"column",justifyContent:"center"}})
+      ], style: {backgroundColor: BACKGROUND_COLOR, display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}
     })
   }
 
